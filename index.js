ec9646571078b42edd8603bbbe532a673d017b01
Noah King <noahking123@yahoo.com>
2022-08-09 15:23:24 -0400

    commit tree 65bcd8d3c4591fe49e02d08d0fc6d3d812632ea6

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const drivers = ['Antonia','Nuru','Amari','Mo'];
+
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = (divers) => {
+    return drivers.slice(2,4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+ 
+/*function createFareMultiplier() {
+    return function(x) {
+        console.log(arguments[0]);
+        return x * x;
+    }
+}*/
+
+const createFareMultiplier = (number) => {
+    return function(x) {
+        return x * number;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2); 
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers,func1) {
+    return func1(drivers);
+}
